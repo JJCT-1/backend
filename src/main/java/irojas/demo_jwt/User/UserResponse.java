effed4bcfe1db4f5affb8856b5adab193d4f644e
Johan Cruzado <cruzadotorres2012@gmail.com>
2024-06-18 05:47:55 -0500

    ultimo cambio para login

---
+++ src/main/java/irojas/demo_jwt/User/UserResponse.java
@@ -0,0 +1,14 @@
+package irojas.demo_jwt.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    String message;
+}
